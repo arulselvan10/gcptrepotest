0f7b263d45cd60bbf2676d8fe0a480adc3270a7b
arulselvan10 <145689653+arulselvan10@users.noreply.github.com>
2025-07-26 18:22:22 +0530

    Update variables.tf

--- terraform/clusters/dev/variables.tf
+++ terraform/clusters/dev/variables.tf
@@ -1,9 +1,24 @@
-variable "config_name" {}
-variable "cluster_name" {}
-variable "resource_group_name" {}
-variable "operator_namespace" {}
-variable "repository_url" {}
-variable "repository_branch" {}
-variable "repository_path" {}
-variable "sync_interval" {}
-variable "timeout" {}
+variable "config_name" { type = string }
+variable "cluster_id" { type = string }
+variable "operator_namespace" { type = string }
+variable "scope" { type = string }                 # e.g. "cluster"
+
+variable "repository_url" { type = string }
+variable "repository_branch_type" { type = string } # branch, tag, commit
+variable "repository_branch" { type = string }
+
+variable "git_sync_interval" { type = number }
+variable "git_timeout" { type = number }
+
+variable "repository_path" { type = string }
+
+variable "sync_interval" { type = number }
+variable "timeout" { type = number }
+variable "retry_interval" { type = number }
+variable "garbage_collection" { type = bool }
+variable "wait" { type = bool }
+
+variable "depends_on" {
+  type    = list(any)
+  default = []
+}
