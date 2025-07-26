a76e5cce7e23891065609f583119d2832a7793a9
arulselvan10 <145689653+arulselvan10@users.noreply.github.com>
2025-07-26 18:16:44 +0530

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,22 +1,26 @@
 resource "azurerm_kubernetes_flux_configuration" "this" {
-  name        = var.config_name
-  cluster_id  = var.cluster_id
-  namespace   = var.operator_namespace
-  scope       = "cluster"
-  source_kind = "GitRepository"
-  suspend     = false
+  name       = var.config_name
+  cluster_id = var.cluster_id
+  namespace  = var.operator_namespace
+  scope      = var.scope
 
   git_repository {
-    url            = var.repository_url
-    reference_type = "branch"
-    reference_name = var.repository_branch
+    url                     = var.repository_url
+    reference_type          = var.repository_branch_type
+    reference_value         = var.repository_branch
+    sync_interval_in_seconds = var.git_sync_interval
+    timeout_in_seconds      = var.git_timeout
   }
 
   kustomizations {
-    name     = "apps"
-    path     = var.repository_path
-    prune    = true
-    interval = "${var.sync_interval}s"
-    timeout  = "${var.timeout}s"
+    name                        = "apps"
+    path                        = var.repository_path
+    sync_interval_in_seconds    = var.sync_interval
+    timeout_in_seconds          = var.timeout
+    retry_interval_in_seconds   = var.retry_interval
+    garbage_collection_enabled  = var.garbage_collection
+    wait                        = var.wait
   }
+
+  depends_on = var.depends_on
 }
