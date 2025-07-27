f3f2ade012f80abbe317e623d66dcadfbc758d25
arulselvan10 <145689653+arulselvan10@users.noreply.github.com>
2025-07-27 17:15:53 +0530

    Update variables.tf

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -7,7 +7,19 @@ variable "clusters" {
 }
 
 variable "gitops_repo_url" { type = string }
-variable "github_pat" { type = string, sensitive = true }
-variable "use_key_vault" { type = bool, default = false }
-variable "key_vault_name" { type = string, default = "" }
-variable "github_pat_secret_name" { type = string, default = "" }
+variable "github_pat" { 
+type = string
+sensitive = true 
+}
+variable "use_key_vault" {
+type = bool
+default = false 
+}
+variable "key_vault_name" {
+type = string
+default = "" 
+}
+variable "github_pat_secret_name" {
+type = string
+default = "" 
+}
