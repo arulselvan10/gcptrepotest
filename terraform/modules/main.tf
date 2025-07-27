724d9495060563998f35d9ad91d1138598f39d4a
arulselvan10 <145689653+arulselvan10@users.noreply.github.com>
2025-07-27 17:07:03 +0530

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,15 +1,3 @@
-data "azurerm_client_config" "current" {}
-
-data "azurerm_key_vault_secret" "github_pat" {
-  count        = var.use_key_vault ? 1 : 0
-  name         = var.github_pat_secret_name
-  key_vault_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/rg-gitops/providers/Microsoft.KeyVault/vaults/${var.key_vault_name}"
-}
-
-locals {
-  pat_value = var.use_key_vault ? data.azurerm_key_vault_secret.github_pat[0].value : var.github_pat
-}
-
 resource "azurerm_kubernetes_flux_configuration" "gitops" {
   for_each                     = { for cluster in var.clusters : cluster.name => cluster }
   name                         = "gitops-config"
