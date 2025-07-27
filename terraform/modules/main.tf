d98467a2a58703563d996aba8f4bd04f1bdfe2c8
arulselvan10 <145689653+arulselvan10@users.noreply.github.com>
2025-07-27 15:43:57 +0530

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,24 +1,25 @@
-resource "azurerm_kubernetes_flux_configuration" "this" {
-  name        = var.config_name
-  cluster_id  = var.cluster_id
-  namespace   = var.operator_namespace
-  scope       = var.scope
+data "azurerm_client_config" "current" {}
 
-  git_repository {
-    url                     = var.repository_url
-    reference_type          = var.repository_branch_type
-    reference_value         = var.repository_branch
-    sync_interval_in_seconds = var.git_sync_interval
-    timeout_in_seconds      = var.git_timeout
-  }
+data "azurerm_key_vault_secret" "github_pat" {
+  count        = var.use_key_vault ? 1 : 0
+  name         = var.github_pat_secret_name
+  key_vault_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/rg-gitops/providers/Microsoft.KeyVault/vaults/${var.key_vault_name}"
+}
 
-  kustomizations {
-    name                        = "apps"
-    path                        = var.repository_path
-    sync_interval_in_seconds    = var.sync_interval
-    timeout_in_seconds          = var.timeout
-    retry_interval_in_seconds   = var.retry_interval
-    garbage_collection_enabled  = var.garbage_collection
-    wait                        = var.wait
-  }
+locals {
+  pat_value = var.use_key_vault ? data.azurerm_key_vault_secret.github_pat[0].value : var.github_pat
+}
+
+resource "azurerm_kubernetes_flux_configuration" "gitops" {
+  for_each                     = { for cluster in var.clusters : cluster.name => cluster }
+  name                         = "gitops-config"
+  cluster_id                   = "/subscriptions/${each.value.subscription_id}/resourceGroups/${each.value.resource_group}/providers/Microsoft.Kubernetes/connectedClusters/${each.key}"
+  namespace                    = "flux-system"
+  scope                        = "cluster"
+  source_kind                  = "GitRepository"
+  git_repository_url           = var.gitops_repo_url
+  git_repository_reference     = { branch = "main" }
+  git_repository_auth_type     = "Token"
+  git_repository_token         = local.pat_value
+  sync_interval_in_seconds     = 60
 }
